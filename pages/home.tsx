c37ba8588360f155ea17867c321d78569634f9fc
Matt Chapman <matt@mattchapman.io>
2021-07-06 09:49:57 +1200

    Add social icons

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,3 +1,5 @@
+import Head from "next/head";
+
 const Header = () => {
   return (
     <header className="text-gray-600 body-font">
@@ -290,9 +292,12 @@ const Banner = () => {
   );
 };
 
-const SocialBox = ({ title, description }) => {
+const SocialBox = ({ title, description, icon }) => {
   return (
-    <div className="flex flex-col w-1/5 py-16 text-center bg-white hover:bg-gray-100 rounded-xl">
+    <div className="flex flex-col w-1/5 py-12 text-center bg-white hover:bg-gray-100 rounded-xl">
+      <div className="p-2">
+        <i className={`fab fa-${icon} fa-2x`} />
+      </div>
       <h3 className="text-xl font-semibold text-black">{title}</h3>
       <p className="w-5/6 mx-auto">{description}</p>
     </div>
@@ -307,10 +312,26 @@ const Social = () => {
           Follow me on social
         </h3>
         <div className="flex flex-wrap justify-center space-x-5">
-          <SocialBox title="GitHub" description="Code and stuff" />
-          <SocialBox title="LinkedIn" description="All about the synergy" />
-          <SocialBox title="Youtube" description="Lot's of country music" />
-          <SocialBox title="Instagram" description="Climbing adventures" />
+          <SocialBox
+            title="GitHub"
+            icon="github"
+            description="Code and stuff"
+          />
+          <SocialBox
+            title="LinkedIn"
+            icon="linkedin"
+            description="All about the synergy"
+          />
+          <SocialBox
+            title="Youtube"
+            icon="youtube"
+            description="Lot's of country music"
+          />
+          <SocialBox
+            title="Instagram"
+            icon="instagram"
+            description="Climbing adventures"
+          />
         </div>
       </div>
     </section>
@@ -320,6 +341,12 @@ const Social = () => {
 export default function Home() {
   return (
     <div className="debug-screens">
+      <Head>
+        <script
+          src="https://kit.fontawesome.com/75363efdbb.js"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
       <Header />
       <Banner />
       <Content>
