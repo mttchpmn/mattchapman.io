0fa85cd9e465d32d2ad7581d7c9424cef73b1fdf
Matt Chapman <matt@mattchapman.io>
2021-07-05 19:59:53 +1200

    Update home POC

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
     // Use *.tsx if using TypeScript
     './pages/**/*.js',
     './components/**/*.js'
+  ],
+  plugins: [
+    require('tailwindcss-debug-screens'),
   ]
   // ...
 }
