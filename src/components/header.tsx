74c9133c774a4d6b6eea17574b7f6630a0ca98ea
Matt Chapman <matt@mattchapman.io>
2021-07-08 11:18:11 +1200

    Update header margins

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,19 +3,19 @@ import Link from "next/link";
 const Header = () => {
   return (
     <header className="text-gray-600 body-font">
-      <div className="flex flex-col flex-wrap items-center p-5 mx-auto md:flex-row">
-        <a className="flex items-center mb-4 text-xl font-semibold text-gray-900 title-font md:mb-0">
+      <div className="flex flex-col flex-wrap items-center p-2 mx-auto md:p-5 md:flex-row">
+        <a className="flex items-center mb-2 text-xl font-semibold text-gray-900 md:mb-4 title-font md:mb-0">
           Matt Chapman
         </a>
         <nav className="flex flex-wrap items-center justify-center text-base md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400">
           <Link href="/">
-            <a className="mr-5 hover:text-gray-900">About</a>
+            <a className="mr-5 hover:text-indigo-900">About</a>
           </Link>
           <Link href="/#contact">
-            <a className="mr-5 hover:text-gray-900">Contact</a>
+            <a className="mr-5 hover:text-indigo-900">Contact</a>
           </Link>
           <Link href="/blog">
-            <a className="mr-5 hover:text-gray-900">Blog</a>
+            <a className="mr-5 hover:text-indigo-900">Blog</a>
           </Link>
         </nav>
       </div>
