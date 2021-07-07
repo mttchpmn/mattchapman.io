e7a6837b035ce1f9a46d7c2bc74457e091e09211
Matt Chapman <matt@mattchapman.io>
2021-07-08 11:20:00 +1200

    Update header margins

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header className="text-gray-600 body-font">
       <div className="flex flex-col flex-wrap items-center p-2 mx-auto md:p-5 md:flex-row">
-        <a className="flex items-center mb-2 text-xl font-semibold text-gray-900 md:mb-4 title-font md:mb-0">
+        <a className="flex items-center mb-2 text-xl font-semibold text-gray-900 title-font md:mb-0">
           Matt Chapman
         </a>
         <nav className="flex flex-wrap items-center justify-center text-base md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400">
