22dce685c0ba421a77480e0099fae60b64215769
Matt Chapman <matt@mattchapman.io>
2021-07-09 18:25:34 +1200

    Add sendgrid

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -1,7 +1,29 @@
 import { NextApiResponse } from "next";
 import { NextApiRequestQuery } from "next/dist/next-server/server/api-utils";
+// import "dotenv/config";
+const sgMail = require("@sendgrid/mail");
 
 export default (req: NextApiRequestQuery, res: NextApiResponse) => {
   console.log(req.body);
-  res.status(200).json({ hello: "world" });
+
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+  console.log("$" + JSON.stringify(process.env, null, 2));
+
+  const msg = {
+    to: "matt.chapman@firstaml.co.nz", // Change to your recipient
+    from: "matt@mattchapman.io", // Change to your verified sender
+    subject: "Ola compadre",
+    text: "and easy to do anywhere, even with Node.js",
+    html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+  };
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log("Email sent");
+      res.status(200).json({ hello: "world" });
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 };
