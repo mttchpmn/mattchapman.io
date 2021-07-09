d18214b6d95d303517c2cd40265fc5252bde8a7c
Matt Chapman <matt@mattchapman.io>
2021-07-10 10:41:46 +1200

    Update contact form API

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -2,26 +2,29 @@ import { NextApiResponse } from "next";
 import { NextApiRequestQuery } from "next/dist/next-server/server/api-utils";
 
 const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-export default (req: NextApiRequestQuery, res: NextApiResponse) => {
-  console.log(req.body);
-
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+export default async (req: NextApiRequestQuery, res: NextApiResponse) => {
+  const { name, email, message } = JSON.parse(req.body as any);
 
   const msg = {
-    to: "matt.chapman@firstaml.co.nz", // Change to your recipient
-    from: "matt@mattchapman.io", // Change to your verified sender
-    subject: "Ola compadre",
-    text: "and easy to do anywhere, even with Node.js",
-    html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+    to: process.env.CONTACT_EMAIL_ADDRESS,
+    from: "contact@mattchapman.io", // Change to your verified sender
+    subject: "New website contact message received",
+    text: `Sender Name: ${name}\nSender Email: ${email}\nMessage:\n${message}`,
+    html: `<p>Sender Name: ${name}</p><p>Sender Email: <a href="mailto: ${email}">${email}</a></p><div><p>Message:<p/>${message}</div>`,
   };
-  sgMail
-    .send(msg)
-    .then(() => {
-      console.log("Email sent");
-      res.status(200).json({ hello: "world" });
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+
+  try {
+    await sgMail.send(msg);
+    console.log("email sent");
+
+    res
+      .status(200)
+      .json({ successful: true, message: "Email sent successfully" });
+  } catch (error) {
+    console.log("Failed to send email");
+
+    res.status(500).json({ successful: false, message: error.message });
+  }
 };
