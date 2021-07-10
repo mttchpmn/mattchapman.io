b3857b0392a288c647dbc7161a68b551b87faa0f
Matt Chapman <matt@mattchapman.io>
2021-07-10 13:24:18 +1200

    Fix meta tag

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -8,10 +8,11 @@ const Layout = ({ children }) => {
     <>
       <Head>
         <title>Matt Chapman</title>
-        <meta name="description">
-          Matt Chapman is a software engineer, commercial pilot, musician, and
-          adventurer, from the mountainous wilds of New Zealand.
-        </meta>
+        <meta
+          name="description"
+          content="Matt Chapman is a software engineer, commercial pilot, musician, and
+          adventurer, from the mountainous wilds of New Zealand."
+        />
         <link
           href="https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css"
           rel="stylesheet"
