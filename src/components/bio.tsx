dd4948e5956373a6504b47413abe83febbf42ca5
Matt Chapman <matt@mattchapman.io>
2021-07-10 14:07:42 +1200

    use Next image

--- src/components/bio.tsx
+++ src/components/bio.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
+import Image from "next/image";
 
 import { AboutContent } from "../lib/datocms";
 
 export default function Bio({ content }: { content: AboutContent }) {
   return (
     <section className="text-gray-600 body-font bg-grey-500">
-      <div className="flex flex-col items-center max-w-6xl px-5 py-24 mx-auto md:flex-row">
+      <div className="flex flex-col items-center max-w-6xl px-5 py-16 mx-auto md:py-24 md:flex-row">
         <div className="flex flex-col items-center mb-16 text-center lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 md:items-start md:text-left md:mb-0">
           <h1 className="mb-4 text-3xl font-medium text-gray-900 title-font sm:text-4xl">
             {content.tagline}
@@ -19,12 +20,8 @@ export default function Bio({ content }: { content: AboutContent }) {
             </Link>
           </div>
         </div>
-        <div className="w-5/6 lg:max-w-lg lg:w-full md:w-1/2">
-          <img
-            className="object-cover object-center rounded"
-            alt="Picture of Matt Chapman"
-            src={content.url}
-          />
+        <div className="relative w-11/12 h-64 lg:max-w-lg lg:w-full md:w-1/2">
+          <Image src={content.url} layout="fill" objectFit="cover" />
         </div>
       </div>
     </section>
