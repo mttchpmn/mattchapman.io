c908dc1fa93a46af26ff8df2d31099395dc82802
Matt Chapman <matt@mattchapman.io>
2021-07-13 19:50:29 +1200

    Make home link

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,9 +4,11 @@ const Header = () => {
   return (
     <header className="text-gray-600 body-font">
       <div className="flex flex-col flex-wrap items-center p-2 mx-auto md:p-5 md:flex-row">
-        <a className="flex items-center mb-2 text-xl font-semibold text-gray-900 title-font md:mb-0">
-          Matt Chapman
-        </a>
+        <Link href="/">
+          <a className="flex items-center mb-2 text-xl font-semibold text-gray-900 title-font md:mb-0">
+            Matt Chapman
+          </a>
+        </Link>
         <nav className="flex flex-wrap items-center justify-center text-base md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400">
           <Link href="/">
             <a className="mr-5 hover:text-indigo-900">About</a>
