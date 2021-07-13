a259880c2c717c74cc94a2a15c72bfd4162cb834
Matt Chapman <matt@mattchapman.io>
2021-07-13 19:27:59 +1200

    Add width prop

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -1,10 +1,16 @@
-const Content = ({ children, background = "", foreground = "" }) => {
+const Content = ({
+  children,
+  background = null,
+  foreground = null,
+  maxWidth = null,
+}) => {
   const bg = background ? "bg-" + background : "";
   const fg = foreground ? "bg-" + foreground : "";
+  const width = maxWidth ? "max-w-" + maxWidth : "max-w-6xl";
 
   return (
     <div className={`w-full h-full ${bg}`}>
-      <div className={`max-w-6xl px-8 mx-auto ${fg}`}>{children}</div>
+      <div className={`${width} px-8 mx-auto ${fg}`}>{children}</div>
     </div>
   );
 };
