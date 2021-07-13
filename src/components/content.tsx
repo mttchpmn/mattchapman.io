550f17e8a683f555295e34d8b547892c6a450281
Matt Chapman <matt@mattchapman.io>
2021-07-13 18:50:18 +1200

    Update content component

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -1,7 +1,12 @@
-const Content = ({ children }) => (
-  <div className="container max-w-sm min-w-full min-h-screen bg-gray-100 md:max-w-6xl">
-    <div className="bg-gradient-to-t from-blue-50 to-gray-100">{children}</div>
-  </div>
-);
+const Content = ({ children, background = "", foreground = "" }) => {
+  const bg = background ? "bg-" + background : "";
+  const fg = foreground ? "bg-" + foreground : "";
+
+  return (
+    <div className={`w-full h-full ${bg}`}>
+      <div className={`max-w-6xl px-8 mx-auto ${fg}`}>{children}</div>
+    </div>
+  );
+};
 
 export default Content;
