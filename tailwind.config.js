02086bf4c6e42a3a11fa2d2116d6af0642a4f480
Matt Chapman <matt@mattchapman.io>
2021-07-13 19:27:34 +1200

    Extend height scale

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  theme: {
+    extend: {
+      height: theme => ({
+        '500': '500px',
+        '750': '750px',
+      })
+    }
+  },
   purge: [
     // Use *.tsx if using TypeScript
     './src/pages/**/*.tsx',
