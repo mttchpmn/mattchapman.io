05ef23aac3720ac8a2fe01a311c3f43d44661b71
Matt Chapman <matt@mattchapman.io>
2021-07-17 16:35:05 +1200

    Fix content

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -9,7 +9,7 @@ const Content = ({
   const width = maxWidth ? maxWidth : "max-w-6xl";
 
   return (
-    <div className={`w-screen h-screen ${bg}`}>
+    <div className={`w-full h-full ${bg}`}>
       <div className={`${width} px-2 md:px-8 mx-auto ${fg}`}>{children}</div>
     </div>
   );
