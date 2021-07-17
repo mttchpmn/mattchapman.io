9da6c211c61cf0ae62f7dc77f7171022c5c487d7
Matt Chapman <matt@mattchapman.io>
2021-07-18 09:55:46 +1200

    Create card component

---
+++ src/components/ui/card.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+import Link from "next/link";
+
+type CardProps = {
+  title: string;
+  description: string;
+  category: string;
+  img: string;
+  href: string;
+  cta: string;
+};
+
+const Card: React.FC<CardProps> = (props) => {
+  return (
+    <div className="w-full h-full p-4">
+      <div className="bg-white border-2 border-gray-200 rounded-lg border-opacity-60">
+        {/* Card media */}
+        <div className="relative h-40 bg-blue-500 rounded-lg sm:h-64 lg:h-52">
+          <Image src={props.img} layout="fill" objectFit="cover" />
+        </div>
+
+        {/* Card content */}
+        <div className="flex flex-col justify-between h-64 p-6 md:h-64">
+          <div>
+            <p className="text-sm text-gray-400">{props.category}</p>
+            <div className="py-2">
+              <h3 className="text-lg text-black">{props.title}</h3>
+            </div>
+            <div className="text-gray-700">{props.description}</div>
+          </div>
+          <Link href={props.href}>
+            <a>
+              <div className="flex text-purple-500 hover:text-purple-700">
+                <p>{props.cta}</p>
+                <i className="bx bxs-right-arrow-alt bx-sm"></i>
+              </div>
+            </a>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
