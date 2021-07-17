c072d69b7d3dfc8b8d6e09399214da8870b3f857
Matt Chapman <matt@mattchapman.io>
2021-07-17 17:58:21 +1200

    Update modal

--- src/components/ui/modal.tsx
+++ src/components/ui/modal.tsx
@@ -29,7 +29,7 @@ const Modal = ({ title, visible, setVisible, children }) => {
           </div>
           <div className="p-8 bg-white">{children}</div>
           <hr />
-          <div className="flex justify-end px-4 py-4 text-white">
+          <div className="flex justify-end px-4 py-2 text-white">
             <Button text="Ok" onClick={() => setVisible(false)} />
           </div>
         </div>
