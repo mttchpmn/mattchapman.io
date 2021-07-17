abb78415e63b1fcd1cdb9f7b9a198d29c38022ca
Matt Chapman <matt@mattchapman.io>
2021-07-18 11:13:00 +1200

    Update content

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -1,16 +1,16 @@
-const Content = ({
-  children,
-  background = null,
-  foreground = null,
-  maxWidth = null,
-}) => {
-  const bg = background ? "bg-" + background : "";
-  const fg = foreground ? "bg-" + foreground : "";
+import { ReactNode } from "react";
+
+type ContentProps = {
+  children: ReactNode;
+  maxWidth: string;
+};
+
+const Content: React.FC<ContentProps> = ({ children, maxWidth }) => {
   const width = maxWidth ? maxWidth : "max-w-6xl";
 
   return (
-    <div className={`${bg}`}>
-      <div className={`${width} px-2 md:px-8 mx-auto ${fg}`}>{children}</div>
+    <div className={``}>
+      <div className={`${width} px-2 md:px-8 mx-auto`}>{children}</div>
     </div>
   );
 };
