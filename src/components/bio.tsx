b6e19e08396b2c8d80279ab4d0a8f2b42e84e69c
Matt Chapman <matt@mattchapman.io>
2021-07-18 16:19:32 +1200

    Use new title component

--- src/components/bio.tsx
+++ src/components/bio.tsx
@@ -3,15 +3,14 @@ import Image from "next/image";
 
 import { AboutContent } from "../lib/datocms";
 import Button from "./ui/button";
+import Title from "./ui/title";
 
 export default function Bio({ content }: { content: AboutContent }) {
   return (
     <section className="text-gray-600 body-font bg-grey-500">
       <div className="flex flex-col items-center max-w-6xl px-5 py-16 mx-auto md:py-24 md:flex-row">
         <div className="flex flex-col items-center mb-16 text-center lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 md:items-start md:text-left md:mb-0">
-          <h1 className="mb-4 text-3xl font-medium text-gray-900 title-font sm:text-4xl">
-            {content.tagline}
-          </h1>
+          <Title variant={1} text={content.tagline} />
           <p className="mb-8 leading-relaxed">{content.description}</p>
           <div className="flex justify-center w-full md:justify-start">
             <Link href="#contact">
